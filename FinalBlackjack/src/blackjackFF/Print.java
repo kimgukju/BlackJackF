db379f4f9e0978a7e7fe6df43b13d87681164fe8
kimgukju <kimgukju@kimgukju.com>
2023-05-18 23:22:56 +0900

    add_color

--- FinalBlackjack/src/blackjackFF/Print.java
+++ FinalBlackjack/src/blackjackFF/Print.java
@@ -13,7 +13,7 @@ public class Print {
 	public Print() {
 		// TODO Auto-generated constructor stub
 		//game = new BlackjackGame();  
-		// 이런식으로  bjgame,print 문에 서로 선언해서 써버리면 StackOverflowError 남 
+		// 이런식으로  bjgame,print 클래스에 서로 선언해서 써버리면 StackOverflowError 남 
 		// 그래서 bjgame 에 public static 으로 바꾸고 여기서 bjgame.~~ 식으로 호출해서 프린트하게함 
 		playerHand = new ArrayList<String>();
 		dealerHand = new ArrayList<String>();
@@ -21,16 +21,66 @@ public class Print {
 		dealerScore = 0;
 	}
 	
+//public static void main(String[] args) {
+//	String[] blackCard = new String[14];
+//	blackCard[0] = "♠A";
+//	for(int i=1; i<11; i++) {
+//		blackCard[i] =  "♠"+(i);
+//		}
+//	blackCard[11] = "♠J";
+//	blackCard[12] = "♠Q";
+//	blackCard[13] = "♠K";
+//
+//	for(int i=0; i<blackCard.length; i++) {
+//		System.out.println(blackCard[i]);
+//	}
+//		
+//	}
+//	
+//
+//	
+//	public void cardColor() {
+//		String[] blackCard = new String[13];
+//		blackCard[0] = "♠1";
+//		for(int i=0; i<blackCard.length; i++) {
+//			blackCard[i] = "♠1"+i;
+//		}
+//	}
+
+	
+	/*
+	 *    ♠ 포함하면 검정색
+	 *    ♥ 포함하면 빨광색
+	 *    ♦ 포함하면 보라색
+	 *    ♣ 포함하면 초록색
+	 *    단, 10은 2자리수라 범위차지해서 따로 카드폼 만들어주셔서 또 따로 선언
+	 */
+	
 	public void printPlayerHand() {
 		System.out.print("            " + "┌───┐ ".repeat(BlackjackGame.playerHand.size()) + "\n");
 		System.out.print("유저 손패 : ");
 		for (int i = 0; i < BlackjackGame.playerHand.size(); i++) {
-			if (BlackjackGame.playerHand.get(i).equals("♠10") || BlackjackGame.playerHand.get(i).equals("♥10") || BlackjackGame.playerHand.get(i).equals("♦10")
-					|| BlackjackGame.playerHand.get(i).equals("♣10")) {
-				System.out.print("│" + BlackjackGame.playerHand.get(i) + "│ ");
-			} else {
-				System.out.print("│ " + BlackjackGame.playerHand.get(i) + "│ ");
+			if (BlackjackGame.playerHand.get(i).equals("♠10")) {
+				System.out.print("│" + AnsiConsol.BLACK(BlackjackGame.playerHand.get(i)) + "│ ");
+			} else if(BlackjackGame.playerHand.get(i).equals("♥10")) {
+				System.out.print("│" + AnsiConsol.BLACK(BlackjackGame.playerHand.get(i)) + "│ ");
+			} else if(BlackjackGame.playerHand.get(i).equals("♦10")) {
+				System.out.print("│" + AnsiConsol.BLACK(BlackjackGame.playerHand.get(i)) + "│ ");
+			} else if(BlackjackGame.playerHand.get(i).equals("♣10")) {
+				System.out.print("│" + AnsiConsol.BLACK(BlackjackGame.playerHand.get(i)) + "│ ");
 			}
+			else if(BlackjackGame.playerHand.get(i).contains("♠")) {
+				System.out.print("│ " + AnsiConsol.BLACK(BlackjackGame.playerHand.get(i))+ "│ ");
+			} else if(BlackjackGame.playerHand.get(i).contains("♥")) {
+				System.out.print("│ " + AnsiConsol.RED(BlackjackGame.playerHand.get(i))+ "│ ");
+			}
+			else if(BlackjackGame.playerHand.get(i).contains("♦")) {
+				System.out.print("│ " + AnsiConsol.BLUE(BlackjackGame.playerHand.get(i))+ "│ ");
+			}
+			else if(BlackjackGame.playerHand.get(i).contains("♣")) {
+				System.out.print("│ " + AnsiConsol.GREEN(BlackjackGame.playerHand.get(i))+ "│ ");
+			}
+			
 		}
 		System.out.print("점수: " + BlackjackGame.playerScore);
 		System.out.println();
@@ -41,11 +91,31 @@ public class Print {
 		System.out.print("            " + "┌───┐ ".repeat(BlackjackGame.dealerHand.size()) + "\n");
 		System.out.print("딜러 손패 : ");
 		for (int i = 0; i < BlackjackGame.dealerHand.size(); i++) {
-			if (BlackjackGame.dealerHand.get(i).equals("♠10") || BlackjackGame.dealerHand.get(i).equals("♥10") ||BlackjackGame.dealerHand.get(i).equals("♦10")
-					|| BlackjackGame.dealerHand.get(i).equals("♣10")) {
-				System.out.print("│" + BlackjackGame.dealerHand.get(i) + "│ ");
-			} else {
-				System.out.print("│ " + BlackjackGame.dealerHand.get(i) + "│ ");
+//			if (BlackjackGame.dealerHand.get(i).equals("♠10") || BlackjackGame.dealerHand.get(i).equals("♥10") ||BlackjackGame.dealerHand.get(i).equals("♦10")
+//					|| BlackjackGame.dealerHand.get(i).equals("♣10")) {
+//				System.out.print("│" + BlackjackGame.dealerHand.get(i) + "│ ");
+//			} else {
+//				System.out.print("│ " + BlackjackGame.dealerHand.get(i) + "│ ");
+//			}
+			if (BlackjackGame.dealerHand.get(i).equals("♠10")) {
+				System.out.print("│" + AnsiConsol.BLACK(BlackjackGame.dealerHand.get(i)) + "│ ");
+			} else if(BlackjackGame.dealerHand.get(i).equals("♥10")) {
+				System.out.print("│" + AnsiConsol.BLACK(BlackjackGame.dealerHand.get(i)) + "│ ");
+			} else if(BlackjackGame.dealerHand.get(i).equals("♦10")) {
+				System.out.print("│" + AnsiConsol.BLACK(BlackjackGame.dealerHand.get(i)) + "│ ");
+			} else if(BlackjackGame.dealerHand.get(i).equals("♣10")) {
+				System.out.print("│" + AnsiConsol.BLACK(BlackjackGame.dealerHand.get(i)) + "│ ");
+			}
+			else if(BlackjackGame.dealerHand.get(i).contains("♠")) {
+				System.out.print("│ " + AnsiConsol.BLACK(BlackjackGame.dealerHand.get(i))+ "│ ");
+			} else if(BlackjackGame.dealerHand.get(i).contains("♥")) {
+				System.out.print("│ " + AnsiConsol.RED(BlackjackGame.dealerHand.get(i))+ "│ ");
+			}
+			else if(BlackjackGame.dealerHand.get(i).contains("♦")) {
+				System.out.print("│ " + AnsiConsol.BLUE(BlackjackGame.dealerHand.get(i))+ "│ ");
+			}
+			else if(BlackjackGame.dealerHand.get(i).contains("♣")) {
+				System.out.print("│ " + AnsiConsol.GREEN(BlackjackGame.dealerHand.get(i))+ "│ ");
 			}
 		}
 		System.out.print("점수: " + BlackjackGame.dealerScore);
@@ -56,12 +126,33 @@ public class Print {
 	public void printDealerHand() {
 		System.out.print("            " + "┌───┐ ".repeat(BlackjackGame.dealerHand.size()) + "\n");
 		System.out.print("딜러 손패 : ");
-		if (BlackjackGame.dealerHand.get(0).equals("♠10") || BlackjackGame.dealerHand.get(0).equals("♥10") || BlackjackGame.dealerHand.get(0).equals("♦10")
-				|| BlackjackGame.dealerHand.get(0).equals("♣10")) {
-			System.out.print("│" + BlackjackGame.dealerHand.get(0) + "│ ");
-		} else {
-			System.out.print("│ " + BlackjackGame.dealerHand.get(0) + "│ ");
+//		if (BlackjackGame.dealerHand.get(0).equals("♠10") || BlackjackGame.dealerHand.get(0).equals("♥10") || BlackjackGame.dealerHand.get(0).equals("♦10")
+//				|| BlackjackGame.dealerHand.get(0).equals("♣10")) {
+//			System.out.print("│" + BlackjackGame.dealerHand.get(0) + "│ ");
+//		} else {
+//			System.out.print("│ " + BlackjackGame.dealerHand.get(0) + "│ ");
+//		}
+		if (BlackjackGame.dealerHand.get(0).equals("♠10")) {
+			System.out.print("│" + AnsiConsol.BLACK(BlackjackGame.dealerHand.get(0)) + "│ ");
+		} else if(BlackjackGame.dealerHand.get(0).equals("♥10")) {
+			System.out.print("│" + AnsiConsol.BLACK(BlackjackGame.dealerHand.get(0)) + "│ ");
+		} else if(BlackjackGame.dealerHand.get(0).equals("♦10")) {
+			System.out.print("│" + AnsiConsol.BLACK(BlackjackGame.dealerHand.get(0)) + "│ ");
+		} else if(BlackjackGame.dealerHand.get(0).equals("♣10")) {
+			System.out.print("│" + AnsiConsol.BLACK(BlackjackGame.dealerHand.get(0)) + "│ ");
 		}
+		else if(BlackjackGame.dealerHand.get(0).contains("♠")) {
+			System.out.print("│ " + AnsiConsol.BLACK(BlackjackGame.dealerHand.get(0))+ "│ ");
+		} else if(BlackjackGame.dealerHand.get(0).contains("♥")) {
+			System.out.print("│ " + AnsiConsol.RED(BlackjackGame.dealerHand.get(0))+ "│ ");
+		}
+		else if(BlackjackGame.dealerHand.get(0).contains("♦")) {
+			System.out.print("│ " + AnsiConsol.BLUE(BlackjackGame.dealerHand.get(0))+ "│ ");
+		}
+		else if(BlackjackGame.dealerHand.get(0).contains("♣")) {
+			System.out.print("│ " + AnsiConsol.GREEN(BlackjackGame.dealerHand.get(0))+ "│ ");
+		}
+		
 
 		System.out.print("│ ? │ ".repeat(BlackjackGame.dealerHand.size() - 1));
 		System.out.println();
